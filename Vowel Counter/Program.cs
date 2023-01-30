d0d97552cfed739b9f01631b879b702b418a5f92
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:48:08 -0500

    Add project files.

---
+++ Vowel Counter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Vowel_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type in an English word: ");
+            var input = Console.ReadLine();
+            var frequency = 0;
+            foreach (char c in input)
+            {
+                if (c == 'a')
+                {
+                    frequency++;
+                }
+                if (c == 'e')
+                {
+                    frequency++;
+                }
+                if (c == 'i')
+                {
+                    frequency++;
+                }
+                if (c == 'o')
+                {
+                    frequency++;
+                }
+                if (c == 'u')
+                {
+                    frequency++;
+                }
+            }
+            Console.WriteLine(frequency);
+        }
+    }
+}
